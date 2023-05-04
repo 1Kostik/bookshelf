7d9dbab2901944a26327a51dfe74c6ca6c6719ce
AleksandrFedorov <ol.m.fed@gmail.com>
2023-05-05 02:51:49 +0300

    categories_v1

---
+++ src/js/categories.js
@@ -0,0 +1,37 @@
+// import { markupCategories } from './markupCat.js';
+// import { createOnScreen } from './createOnScreen.js';
+// import { scrollIntoView } from "scroll-js";
+
+const categoriesRef = document.body.querySelector('.categories');
+const categoriesListRef = document.querySelector('.categories-list');
+
+function markupCategories(catArray) {
+  const CAT_URL =
+    'https://books-backend.p.goit.global/books/category?category=';
+  return catArray.reduce((acc, item) => {
+    acc += `<li><a href="${CAT_URL}${item.list_name}">${item.list_name}</a></li>`;
+    return acc;
+  }, '');
+}
+
+function createOnScreen(data) {
+  categoriesListRef.insertAdjacentHTML('beforeend', data);
+}
+
+function getCategories() {
+  const url = 'https://books-backend.p.goit.global/books/category-list';
+  fetch(url)
+    .then(response => response.json())
+    .then(markupCategories)
+    .then(createOnScreen);
+}
+
+getCategories();
+
+categoriesRef.addEventListener('click', onCategoryNameClick);
+
+function onCategoryNameClick(e) {
+  e.preventDefault();
+  let currenUrl = e.target.href;
+  fetch(currenUrl).then(response => response.json());
+}
