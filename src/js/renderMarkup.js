05d6de561ca7679d454fb2ae9707176e33075e43
Alex <117479029+AlexTkachenkoFullstack@users.noreply.github.com>
2023-05-05 17:57:03 +0100

    Update renderMarkup.js

--- src/js/renderMarkup.js
+++ src/js/renderMarkup.js
@@ -1,7 +1,7 @@
 export function makeMarkupBook( books ) {
     const markup=  books.reduce((acc, { _id, book_image, author, title}) => {
      acc+=`<li class="book-item" id="${_id}">
-    <img src="${book_image}" alt="${title}" class="book-img" width="180px" height="256px"/>
+    <img loading="lazy" src="${book_image}" alt="${title}" class="book-img" width="180px" height="256px"/>
         <p class="book-title">${title}</p>
         <p class="author-name">${author}</p>
         </li>
