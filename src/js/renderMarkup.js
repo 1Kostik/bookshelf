cad915c84d7574696aa0e89ba01641250b64faec
Alex <117479029+AlexTkachenkoFullstack@users.noreply.github.com>
2023-05-05 17:31:19 +0100

    bookshelf

--- src/js/renderMarkup.js
+++ src/js/renderMarkup.js
@@ -1,9 +1,9 @@
- export function makeMarkupBook( books ) {
+ export  function makeMarkupBook( books ) {
     const markup=  books.reduce((acc, { _id, book_image, author, title}) => {
      acc+=`<li class="book-item" id="${_id}">
     <img src="${book_image}" alt="${title}" class="book-img" width="180px" height="256px"/>
-        <p>${title}</p>
-        <p>${author}</p>
+        <p class="book-title">${title}</p>
+        <p class="author-name">${author}</p>
         </li>
     `
         return acc
