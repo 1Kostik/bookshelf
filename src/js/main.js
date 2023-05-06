7356798402fa9b476cda8b776dbf63495fa9ede4
Alex <117479029+AlexTkachenkoFullstack@users.noreply.github.com>
2023-05-06 20:52:30 +0100

    bookshelf

--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,43 @@
 const refs = {
     bestsellersSectionEl: document.querySelector('.bookshelf'),
+    categoriesListEl: document.querySelector('.categories-list'),
+    allCategoriesTitleEl: document.querySelector('.cat-list'),
+    bestsellersButton:document.querySelector('.bestsellers-button'),
 }
-
-import {makeMarkupBook} from './renderMarkup'
-
-async function getBestsellersBooks() {
-    try {
-        const response = await fetch('https://books-backend.p.goit.global/books/top-books');
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.log(error.message)
-    }
-}
+var debounce = require('lodash.debounce');
+import { makeMarkupBook } from './renderMarkup';
+import { FetchApiBooks } from './fetchApi';
+const fetchApiBooks = new FetchApiBooks();
 
 
 window.addEventListener('DOMContentLoaded', loadBooksItems);
+refs.allCategoriesTitleEl.addEventListener("click", loadBooksItems);
+
 async function loadBooksItems(event) {
     try {
-        const data = await getBestsellersBooks();
-        makeupBestsellersBooks(data);
-
+        const data = await fetchApiBooks.fetchTopBooks();
+       const makeupBooks=await makeupBestsellersBooks(data);
+       
     } catch (error) {
         console.log(error.message)
     }
 }
 
+ 
+refs.bestsellersSectionEl.addEventListener('click', onButtonCattegoryElClick)
+async function onButtonCattegoryElClick(event) {
+    if (event.target.nodeName !== 'BUTTON') {
+        return
+    }
+    refs.buttonCattegoryEl = document.querySelector('.bestsellers-button');
+    const cattegoryName = event.target.name;
+    const categoreTitleOnMainPage = document.querySelector('.bestsellers-title');
+   const data = await fetchApiBooks.fetchSelectedCategory(cattegoryName);
+    categoreTitleOnMainPage.innerHTML = changeColorStyleInTitle(cattegoryName);
+     makeupCategoryShelf(data, cattegoryName);
+}
+
 async function makeupBestsellersBooks(data) {
-    console.log(data);
     const bestsellersTitle = "Best Sellers Books";
     const markup =await data.reduce((acc, {list_name, books}) => {
        acc += `
@@ -36,7 +46,7 @@ async function makeupBestsellersBooks(data) {
         <ul class="bestsellers-book-list">
         ${makeMarkupBook(books)}
         </ul>
-        <button type="button" class="bestsellers-button">see more</button>
+        <button type="button" class="bestsellers-button" name="${list_name}">see more</button>
         </div>`
         
         return acc
@@ -44,11 +54,38 @@ async function makeupBestsellersBooks(data) {
     refs.bestsellersSectionEl.innerHTML = markup;
 }
 
+
+
 function changeColorStyleInTitle(category) {
     const titleInArray = category.trim().split(" ");
     const lastWord=titleInArray[titleInArray.length-1];
     const stringExceptLastWord = titleInArray.slice(0, titleInArray.length - 1).join(' ');
     const string = stringExceptLastWord + ` <span class="categories-title-last-word">${lastWord}</span>`;
     return string;
+}
+
+
+refs.categoriesListEl.addEventListener('click', loadCategoryBooksOnClick);
+async function loadCategoryBooksOnClick(event) {
+    const nameCategory = event.target.textContent;
+    console.log(nameCategory)
+    const categoreTitleOnMainPage = document.querySelector('.bestsellers-title');
+    console.log(categoreTitleOnMainPage)
+    const data = await fetchApiBooks.fetchSelectedCategory(nameCategory);
+    categoreTitleOnMainPage.innerHTML = changeColorStyleInTitle(nameCategory);
+     makeupCategoryShelf(data, nameCategory);
+   
+}
+
+function makeupCategoryShelf(data, nameCategory) {
+    if (nameCategory === 'All categories') {
+        nameCategory='Best Sellers Books'
+    }
+    refs.bestsellersSectionEl.innerHTML= `<h2 class="bestsellers-title">${changeColorStyleInTitle(nameCategory)}</h2>
+        <div class="bestsellers-category-item" name="${nameCategory}">
+        <ul class="bestsellers-book-list">
+       ${makeMarkupBook(data)}
+        </ul>
+        </div>`
 }
 
