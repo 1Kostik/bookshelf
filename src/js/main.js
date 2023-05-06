b19e97f7fae6f1f520f11b2a770a9f9c8fce07d6
Alex <117479029+AlexTkachenkoFullstack@users.noreply.github.com>
2023-05-06 22:10:05 +0100

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -7,38 +7,47 @@ const refs = {
 var debounce = require('lodash.debounce');
 import { makeMarkupBook } from './renderMarkup';
 import { FetchApiBooks } from './fetchApi';
+import Notiflix from 'notiflix';
 const fetchApiBooks = new FetchApiBooks();
 
 
-window.addEventListener('DOMContentLoaded', loadBooksItems);
-refs.allCategoriesTitleEl.addEventListener("click", loadBooksItems);
+window.addEventListener('DOMContentLoaded', loadTopBooksOnClick);
+refs.allCategoriesTitleEl.addEventListener("click",  loadTopBooksOnClick);
 
-async function loadBooksItems(event) {
+async function  loadTopBooksOnClick(event) {
     try {
         const data = await fetchApiBooks.fetchTopBooks();
-       const makeupBooks=await makeupBestsellersBooks(data);
+        if (data.length === 0 || data === undefined) {
+            Notiflix.Notify.failure("Sorry, we didn't find anything according to your request.");
+           return
+       }
+        makeMarkupTopBooksGallery(data);
        
     } catch (error) {
         console.log(error.message)
     }
 }
 
- 
-refs.bestsellersSectionEl.addEventListener('click', onButtonCattegoryElClick)
-async function onButtonCattegoryElClick(event) {
+refs.bestsellersSectionEl.addEventListener('click', onCattegoryButtonElClick)
+async function onCattegoryButtonElClick(event) {
     if (event.target.nodeName !== 'BUTTON') {
         return
     }
-    refs.buttonCattegoryEl = document.querySelector('.bestsellers-button');
     const cattegoryName = event.target.name;
-    const categoreTitleOnMainPage = document.querySelector('.bestsellers-title');
-   const data = await fetchApiBooks.fetchSelectedCategory(cattegoryName);
-    categoreTitleOnMainPage.innerHTML = changeColorStyleInTitle(cattegoryName);
-     makeupCategoryShelf(data, cattegoryName);
+     try {
+         const data = await fetchApiBooks.fetchSelectedCategory(cattegoryName);
+         if (data.length === 0 || data === undefined) {
+            Notiflix.Notify.failure("Sorry, we didn't find anything according to your request.");
+           return
+       }
+        document.querySelector('.bestsellers-title').innerHTML = changeColorStyleInTitle(cattegoryName);
+         makeMarkupCategoryShelf(data, cattegoryName);
+    } catch (error) {
+        console.log(error.message)
+    }
 }
 
-async function makeupBestsellersBooks(data) {
-    const bestsellersTitle = "Best Sellers Books";
+async function  makeMarkupTopBooksGallery(data) {
     const markup =await data.reduce((acc, {list_name, books}) => {
        acc += `
         <div class="bestsellers-category-item">
@@ -50,37 +59,40 @@ async function makeupBestsellersBooks(data) {
         </div>`
         
         return acc
-    }, `<h2 class="bestsellers-title">${changeColorStyleInTitle(bestsellersTitle)}</h2>`);
+    }, `<h2 class="bestsellers-title">${changeColorStyleInTitle('Best Sellers Books')}</h2>`);
     refs.bestsellersSectionEl.innerHTML = markup;
 }
 
 
-
 function changeColorStyleInTitle(category) {
-    const titleInArray = category.trim().split(" ");
-    const lastWord=titleInArray[titleInArray.length-1];
-    const stringExceptLastWord = titleInArray.slice(0, titleInArray.length - 1).join(' ');
-    const string = stringExceptLastWord + ` <span class="categories-title-last-word">${lastWord}</span>`;
-    return string;
+    const titleToArray = category.trim().split(" ");
+    const lastWord=titleToArray[titleToArray.length-1];
+    const stringExceptLastWord = titleToArray.slice(0, titleToArray.length - 1).join(' ');
+    const stringFinal = stringExceptLastWord + ` <span class="categories-title-last-word">${lastWord}</span>`;
+    return stringFinal;
 }
 
 
 refs.categoriesListEl.addEventListener('click', loadCategoryBooksOnClick);
 async function loadCategoryBooksOnClick(event) {
+    if (event.target.nodeName !== 'LI' || event.target.textContent=== 'All categories') {
+        return
+    }
     const nameCategory = event.target.textContent;
-    console.log(nameCategory)
-    const categoreTitleOnMainPage = document.querySelector('.bestsellers-title');
-    console.log(categoreTitleOnMainPage)
-    const data = await fetchApiBooks.fetchSelectedCategory(nameCategory);
-    categoreTitleOnMainPage.innerHTML = changeColorStyleInTitle(nameCategory);
-     makeupCategoryShelf(data, nameCategory);
-   
+ try {
+     const data = await fetchApiBooks.fetchSelectedCategory(nameCategory);
+     if (data.length === 0 || data === undefined) {
+            Notiflix.Notify.failure("Sorry, we didn't find anything according to your request.");
+           return
+       }
+    document.querySelector('.bestsellers-title').innerHTML = changeColorStyleInTitle(nameCategory);
+     makeMarkupCategoryShelf(data, nameCategory);
+    } catch (error) {
+        console.log(error.message)
+    }
 }
 
-function makeupCategoryShelf(data, nameCategory) {
-    if (nameCategory === 'All categories') {
-        nameCategory='Best Sellers Books'
-    }
+function makeMarkupCategoryShelf(data, nameCategory) {
     refs.bestsellersSectionEl.innerHTML= `<h2 class="bestsellers-title">${changeColorStyleInTitle(nameCategory)}</h2>
         <div class="bestsellers-category-item" name="${nameCategory}">
         <ul class="bestsellers-book-list">
